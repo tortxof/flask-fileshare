dc5b972cb6ed330e70eb8d6e34d382be53d8cb5d
Daniel Jones <tortxof@gmail.com>
2016-04-26 11:25:15 -0400

    Added clipboard.js

--- static/app.js
+++ static/app.js
@@ -2,6 +2,10 @@ var upload_form = document.getElementById('upload_form');
 var content_type_input = document.getElementById('content_type_input')
 var file_input = document.getElementById('file_input');
 
-file_input.addEventListener('change', function(event) {
-  content_type_input.value = file_input.files[0].type;
-});
+if (file_input) {
+  file_input.addEventListener('change', function(event) {
+    content_type_input.value = file_input.files[0].type;
+  });
+}
+
+var clipboard = new Clipboard('.copy-button');
